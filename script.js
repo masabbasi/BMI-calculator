61a494412c7735dbc63baa9c028c5b16f8af4f04
MasAbbasi <masoud.abbasi.71@gmail.com>
2022-04-26 20:21:35 +0430

    Fix Bug

--- script.js
+++ script.js
@@ -9,26 +9,30 @@ weight.addEventListener('input',myResult);
 height.addEventListener('input',myResult);
 
 function myResult () {
-    let calResult = (weight.value/((height.value/100)*(height.value/100))).toFixed(1);
+    let calResult = (weight.value/(Math.pow((height.value/100),2))).toFixed(1);
     result.innerHTML=calResult;
     kgValue.innerHTML=weight.value+' KG';
     cmValue.innerHTML=height.value+' CM';
 
     if (calResult<18.5) {
         category.innerHTML='UnderWeight'
-        result.style.color='#FF8C00'
+        result.style.color='#ffc44d'
 
-    } else if (18.5<calResult<24.9) {
-        category.innerHTML='Normal Weight'
-        result.style.color='green'
+    } else if (calResult>=18.5 && calResult<=24.9) {
+        category.innerHTML='Normal'
+        result.style.color='#0be881'
 
-    } else if (25<calResult<29.9) {
+    } else if (calResult>=25 && calResult<=29.9) {
         category.innerHTML='OverWeight'
-        result.style.color='#FF8C00'
-        
-    } else if (calResult>29.9) {
+        result.style.color='#ff884d'
+
+    } else if (calResult>=30 && calResult<=34.9) {
         category.innerHTML='Obese'
-        result.style.color='red'
+        result.style.color='#ff5e4d'
+
+    } else {
+        category.innerHTML='ExtremelyObese'
+        result.style.color='#FF0000'
     } 
 
 }
